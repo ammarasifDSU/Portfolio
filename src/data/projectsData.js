74694edc05ab9b8758d404965a440e8477026f4c
ammarasifDSU <ammarasif9@gmail.com>
2023-04-26 17:06:40 +0400

    New Project Link

--- src/data/projectsData.js
+++ src/data/projectsData.js
@@ -4,7 +4,7 @@ import three from '../assets/svg/projects/three.svg'
 import four from '../assets/svg/projects/four.svg'
 import five from '../assets/svg/projects/five.svg'
 import six from '../assets/svg/projects/six.svg'
-import seven from '../assets/svg/projects/seven.svg'
+import seven from '../assets/png/burger-logo2.jpg'
 import eight from '../assets/svg/projects/eight.svg'
 import NeRA from "../assets/png/NeRA.jpg"
 import contracrMaster from "../assets/png/contractmaster.jpeg"
@@ -16,7 +16,7 @@ export const projectsData = [
         id: 1,
         projectName: 'NeRA',
         projectDesc: 'This project is developed to help providers navigate the complex requirements in the aged care resident agreement process, NeRA is currently the most widely used aged care resident agreement software across Australia.',
-        tags: ['React', 'HTML', 'CSS','React Strap','Asp.Net','Redux','Bootstrap','Resposive','SaaS'],
+        tags: ['React', 'HTML', 'CSS','React Strap','Asp.Net','Redux','Bootstrap','Responsive','SaaS'],
         code: 'https://e-tools.com.au/product/national-e-tools-resident-agreement/',
         image: NeRA
     },
@@ -24,7 +24,7 @@ export const projectsData = [
         id: 2,
         projectName: 'Contract Master',
         projectDesc: 'This project is committed to helping organizations manage their contracts and tenders, from B2B, entirely online.',
-        tags: ['React', 'HTML', 'CSS','React Strap','Node JS',' React Redux','Bootstrap','Resposive','SaaS','AWS'],
+        tags: ['React', 'HTML', 'CSS','React Strap','Node JS',' React Redux','Bootstrap','Responsive','SaaS','AWS'],
         code: 'https://www.thecontractmaster.com/',
         image: contracrMaster
     },
@@ -32,7 +32,7 @@ export const projectsData = [
         id: 3,
         projectName: 'OffernCodes',
         projectDesc: 'Offerncode.com is meant to save money, people’s precious time and provide them with best quality in every single commodity.',
-        tags: ['React', 'HTML', 'CSS','React Strap','Node JS',' React Redux','Bootstrap','Resposive','SaaS','AWS'],
+        tags: ['React', 'HTML', 'CSS','React Strap','Node JS',' React Redux','Bootstrap','Responsive','SaaS','AWS'],
         code: 'https://www.offerncode.com/',
         image: offerncodes
     },
@@ -40,7 +40,7 @@ export const projectsData = [
         id: 4,
         projectName: 'eRAD',
         projectDesc: 'eRAD helps you successfully manage all your accommodation payment requirements for your aged care facility.',
-        tags: ['React', 'HTML', 'CSS','React Strap','Asp.Net','Redux','Bootstrap','Resposive','SaaS'],
+        tags: ['React', 'HTML', 'CSS','React Strap','Asp.Net','Redux','Bootstrap','Responsive','SaaS'],
         code: 'https://e-tools.com.au/product/e-tools-refundable-accommodation-deposit/',
         image: "https://etools.wpenginepowered.com/wp-content/uploads/2021/06/img-software-170px-erad.png"
     },
@@ -48,7 +48,7 @@ export const projectsData = [
         id: 5,
         projectName: 'eSA',
         projectDesc: 'eSA is designed to assist aged care providers manage and produce compliant supplier and brokered service agreements.',
-        tags: ['React', 'HTML', 'CSS','React Strap','Asp.Net','Redux','Bootstrap','Resposive','SaaS'],
+        tags: ['React', 'HTML', 'CSS','React Strap','Asp.Net','Redux','Bootstrap','Responsive','SaaS'],
         code: 'https://e-tools.com.au/product/supplier-contractor-agreements/',
         image: "https://etools.wpenginepowered.com/wp-content/uploads/2021/06/img-software-170px-esa.png"
     },
@@ -56,19 +56,18 @@ export const projectsData = [
         id: 6,
         projectName: 'Consultify',
         projectDesc: 'Consultify',
-        tags: ['React', 'HTML', 'CSS','React Strap','Redux','Bootstrap','Resposive','SaaS'],
+        tags: ['React', 'HTML', 'CSS','React Strap','Redux','Bootstrap','Responsive','SaaS'],
         code: 'https://643d819090d8530008c1a650--eclectic-fox-ce6a71.netlify.app/',
         image: ""
     },
-    // {
-    //     id: 7,
-    //     projectName: 'Stock Market App',
-    //     projectDesc: 'A simple stock market API app',
-    //     tags: ['React', 'Redux', 'Bootstrap'],
-    //     code: 'https://github.com/hhhrrrttt222111/developer-portfolio',
-    //     demo: 'https://github.com/hhhrrrttt222111/developer-portfolio',
-    //     image: seven
-    // },
+    {
+        id: 7,
+        projectName: 'Yum Burger',
+        projectDesc: 'An Online Burger Cafe',
+        tags: ['React', 'HTML', 'CSS','React Strap','Redux','Bootstrap','Responsive','SaaS'],
+        code: 'https://64491b42b8353579832450db--starlit-dragon-333100.netlify.app/',
+        image: seven
+    },
     // {
     //     id: 8,
     //     projectName: 'Car Pooling System',
